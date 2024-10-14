2de8519348a8187b4b059f591f23d97b2b175f17
joaciv17 <166427943+joaciv17@users.noreply.github.com>
2024-10-14 07:08:45 -0300

    Actividad 1
    Resolución de la actividad

--- actividades/actividad_1/src/main.c
+++ actividades/actividad_1/src/main.c
@@ -2,32 +2,15 @@
 #include "pico/stdlib.h"
 
 /**
- * @brief Programa principal
+* @brief Programa principal
 */
 int main(void) {
-	// Inicializo el USB
-    stdio_init_all();
-	// Habilito el GPIO25 (LED)
-    gpio_init(PICO_DEFAULT_LED_PIN);
-    // GPIO25 como salida
-    gpio_set_dir(PICO_DEFAULT_LED_PIN, GPIO_OUT);
 
-	// Mensaje por USB
-	printf("\nHola mundo!\n");
+stdio_init_all();
+printf("hola Joaquin!");
 
-    while (true) {
-    	// Prendo LED
-        gpio_put(PICO_DEFAULT_LED_PIN, 1);
-        // Mensaje por USB
-        printf("LED prendido\n");
-        // Demora
-        sleep_ms(500);
-        // Apago LED
-        gpio_put(PICO_DEFAULT_LED_PIN, 0);
-		// Mensaje por USB
-		printf("LED apagado\n");
-		// Demora
-        sleep_ms(500);
-    }
-    return 0;
+// Mensaje por USB
+while (true) {
+}
+return 0;
 }
